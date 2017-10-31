9e6bf80007fbe6c9717e2a6ff1d4e82354db0fd7
Jonathan Xu <jyx@jonathans-mbp.dhcp.nd.edu>
2017-10-31 18:08:41 -0400

    Implemented Flask main thread

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from Queue import Queue
+import threading
 
 app = Flask(__name__)
 
@@ -22,5 +23,17 @@ def my_form_post():
 # Global Queue
 q = Queue()
 
-if __name__ == '__main__':
-    app.run()
+class flaskThread (threading.Thread):
+   def __init__(self, threadID, name, appName):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.appName = appName
+   def run(self):
+      self.appName.run()
+      
+# Create new threads
+flaskthread = flaskThread(1, "mainflask", app)
+
+# Start new Threads
+flaskthread.start()
