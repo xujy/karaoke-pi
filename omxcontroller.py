256d50b6640c169a2d083f0063d8c22b2674e733
Jonathan Xu <jxu8@nd.edu>
2017-11-01 19:50:19 -0900

    Added omx python wrapper support to play youtube videos

--- omxcontroller.py
+++ omxcontroller.py
@@ -1,7 +1,11 @@
+from omxplayer.player import OMXPlayer
+from time import sleep
 import os
+import subprocess
 
-def PLAY(URL):
-    playurl = os.popen('youtube-dl -g -f mp4 ' + URL).read()
-    OMXPlayer(playurl)
+def PLAY(url):
+    proc = subprocess.Popen(['youtube-dl','-f','mp4', '-g', url], stdout=subprocess.PIPE)
+    realurl = proc.stdout.read()
+    player = OMXPlayer(realurl.decode("utf-8", "strict")[:-1])
 
 PLAY('https://youtu.be/ZqtyQuXo9zM')
